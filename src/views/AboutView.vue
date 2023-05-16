0f807c5586a7aaeef4eb2c9265ad4456c240cfb3
chenhang <1362320337@qq.com>
2023-05-16 17:50:25 +0800

    安装tailwindcss 和 daisyui

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+       <el-button type="primary" size="large">按钮</el-button>
+      <button class="btn btn-warning">Warning</button>
   </div>
 </template>
 
