24ff88514537140852ee458136968c91e0ca697b
chenhang <1362320337@qq.com>
2023-06-02 17:46:42 +0800

    vue3 setup语法糖 ref,计算属性，监听器

---
+++ src/views/Layout.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {computed, ref, watch} from "vue";
+
+const count= ref("0")
+
+const handleClick=()=>{
+  count.value++
+}
+
+const originList =ref([0,1,2,3,4,5,6,7,8,9,10])
+
+const multList=computed(()=>{
+  return originList.value.filter(item=>originList.value.includes(item*item)
+  )
+})
+
+//TODO
+
+watch(count,(newVal,oldVal)=>{
+  console.log('new and old',newVal,oldVal)
+})
+
+</script>
+
+<template>
+<p>count:{{ count }}</p>
+<p>原始数组{{ originList }}</p>
+<p>乘数组{{ multList }}</p>
+   <button class="btn btn-success" @click="handleClick">按钮</button>
+</template>
+
+<style scoped>
+
+</style>
